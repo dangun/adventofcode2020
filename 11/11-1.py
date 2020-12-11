4766fc7a4a83ba861f38f31d7307db06e0bc6d05
dangun <daniel.gunnarsson123@gmail.com>
2020-12-11 19:46:44 +0100

    Day 11

---
+++ 11/11-1.py
@@ -0,0 +1,42 @@
+import os
+import numpy as np
+
+def process(seats):
+
+    rowrange = range(len(seats))
+    colrange = range(len(seats[0]))
+    
+    while True:
+        change_matrix = np.ones_like(seats)
+        # Check all 8 adjacent locations
+        for row in rowrange:
+            for col in colrange:
+                if seats[row][col] == 0:
+                    continue
+                occupied = 0 
+                for i in (-1, 0, 1):
+                    for j in (-1, 0, 1):
+                        if (row + i in rowrange and 
+                            col + j in colrange and 
+                            not i == j == 0):
+                            if seats[row + i][col + j] == 1:
+                                occupied += 1
+                if (occupied == 0 and seats[row][col] == -1) or (occupied >= 4 and seats[row][col] == 1):
+                    change_matrix[row][col] = -1
+
+        # Check if nothing is gonna change, if yes then return the sum of all occupied seats
+        if np.isin(change_matrix, 1).all():
+            return (seats == 1).sum()
+        seats = change_matrix * seats # Multiply each corresponding element
+    
+seats = [] # 0 = floor, 1 = occupied, -1 = empty
+dir_path = os.path.dirname(os.path.realpath(__file__))
+with open(dir_path + '/' + 'input.txt') as f:
+    row = []
+    for line in f:
+        row =  [-1 if x == 'L' else 0 for x in line.strip()] 
+        seats.append(row)
+
+seats = np.array(seats)
+
+print(f'# of occupied seats: {process(seats)}')
---
+++ 11/11-2.py
@@ -0,0 +1,49 @@
+import os
+import numpy as np
+
+def process(seats):
+
+    rowrange = range(len(seats))
+    colrange = range(len(seats[0]))
+    
+    while True:
+        change_matrix = np.ones_like(seats)
+        # Check first seat in all 8 directions
+        for row in rowrange:
+            for col in colrange:
+                if seats[row][col] == 0:
+                    continue
+                occupied = 0 
+                for i in (-1, 0, 1):    
+                    for j in (-1, 0, 1):
+                        if (row + i in rowrange and 
+                            col + j in colrange and 
+                            not i == j == 0):
+
+                            step = 1
+                            while row+step*i in rowrange and col+step*j in colrange:
+                                if seats[row+step*i][col+step*j] == 1:
+                                    occupied += 1
+                                    break
+                                elif seats[row+step*i][col+step*j] == -1:
+                                    break
+                                step += 1
+                if (occupied == 0 and seats[row][col] == -1) or (occupied >= 5 and seats[row][col] == 1):
+                    change_matrix[row][col] = -1
+
+        # Check if nothing is gonna change, if yes then return the sum of all occupied seats
+        if np.isin(change_matrix, 1).all():
+            return (seats == 1).sum()
+        seats = change_matrix * seats # Multiply each corresponding element
+    
+seats = [] # 0 = floor, 1 = occupied, -1 = empty
+dir_path = os.path.dirname(os.path.realpath(__file__))
+with open(dir_path + '/' + 'input.txt') as f:
+    row = []
+    for line in f:
+        row =  [-1 if x == 'L' else 0 for x in line.strip()] 
+        seats.append(row)
+
+seats = np.array(seats)
+
+print(f'# of occupied seats: {process(seats)}')
